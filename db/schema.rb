46b42ac16ab5627d41fda360626d4fe058dd3296
EC2 Default User <ec2-user@ip-172-31-41-132.us-east-2.compute.internal>
2020-07-12 01:44:51 +0000

    ユーザとの紐づけ以外は大体できました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200531073857) do
+ActiveRecord::Schema.define(version: 20200712000613) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
