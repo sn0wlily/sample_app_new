89066c149b32dd46d01b6b6f65b7cfdcd1175c3a
Yurisn0w <yuri.yuki.6@gmail.com>
2020-07-14 00:28:16 +0900

    kadai error & 微調整

--- app/models/kadai.rb
+++ app/models/kadai.rb
@@ -1,2 +1,7 @@
 class Kadai < ApplicationRecord
+  validates :title,  presence: true, length: { maximum: 32 }
+  validates :etc, presence: true, length: { maximum: 100 }
+  validates :due, presence: true
+  validates :imp, numericality: {greater_than: 0,less_than: 4}#1~3
+  validates :sinko,numericality: {greater_than: 0,less_than: 11}#0~10
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200713133115) do
+ActiveRecord::Schema.define(version: 20200712032455) do
 
   create_table "kadais", force: :cascade do |t|
     t.string "title"
