2707b05a940abe73416a6f692d830cb37901c819
Yurisn0w <yuri.yuki.6@gmail.com>
2020-07-15 20:22:00 +0900

    mailer完成&微調整

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,5 +1,4 @@
 if Rails.env.production?
-  ActionMailer::Base.delivery_erroers = true
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
