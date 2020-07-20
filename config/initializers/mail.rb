77790aa1a77a499e9cd5426f67310aba5c7b50fb
Yurisn0w <yuri.yuki.6@gmail.com>
2020-07-20 13:24:26 +0900

    mailer pass edit

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -11,7 +11,7 @@ if Rails.env.production?
     domain: 'gmail.com',
     port: 587,
     user_name: 'tasksup.1b@gmail.com',
-    password: 'nek02cat22',
+    password: 'xlhmnniwkiqfdnln',
     authentication: 'plain',
     enable_starttls_auto: true
   }
